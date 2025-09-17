cbbc493d4def0dc48863ab2fa6c504db68e14746
João Ferreira <j.pedro.ferreira@ipvc.pt>
2025-09-17 15:30:27 +0100

    add: initial tries

---
+++ android/app/src/main/kotlin/com/example/fwdmobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fwdmobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:fwdmobile/realtime_segmentation.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: RealTimeSegmentation(),
+    );
+  }
+}
